ef6fc16cf4f7fa2501111ed88a4b90fd1ca2cdaa
unknown <tolgahan.cepel@gmail.com>
2021-08-20 13:12:43 +0300

    adjusted readme and version

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+      long_description = fh.read()
+
 setup(
       name='openwid',
-      version='0.1',
+      version='0.1.2',
       url="https://github.com/tolgahancepel/openwid",
       author="Tolgahan Cepel",
       author_email="tolgahan.cepel@gmail.com",
       description='OpenWeather in Dash',
+      long_description = long_description,
+      long_description_content_type="text/markdown",
+      classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+            "Framework :: Dash",
+      ],
       packages=['openwid'],
       zip_safe=False
 )
